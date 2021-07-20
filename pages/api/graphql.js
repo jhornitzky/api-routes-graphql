5349c699d3344d58d06231d8371eab0fe38f7271
jhornitzky <james@marlincommunications.com>
2021-07-20 22:15:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/api/graphql.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from 'apollo-server-micro'
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    users(parent, args, context) {
+      return [{ name: 'Nextjs' }]
+    },
+  },
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()
+
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
